3d21ce3735e21107fd1071c9617197d58bd5bc79
AndrewGalosa <galosaandrew@gmail.com>
2025-06-08 02:38:38 +0300

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,15 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
-export default defineConfig({
-    base: '/Todo-vue/',
-    plugins: [vue(), vueJsx(), vueDevTools()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+    return {
+        base: mode === 'production' ? '/Todo-vue/' : '/',
+        plugins: [vue(), vueJsx(), vueDevTools()],
+
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
         },
-    },
+    };
 });
